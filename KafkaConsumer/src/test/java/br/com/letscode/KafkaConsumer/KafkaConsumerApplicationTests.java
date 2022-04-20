82620a05eb0c34de03daa30432ddfdc587b305d6
Rapha Mendonca <raphamendonca@protonmail.com>
2022-04-20 20:30:28 -0300

    Adiocionado projetos do consumer

---
+++ KafkaConsumer/src/test/java/br/com/letscode/KafkaConsumer/KafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.letscode.KafkaConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
